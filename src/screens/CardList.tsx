13e2b638136b2f708e5370e4df1f07a9d3f0fb44
Park EunJIn <jindongkk888@gmail.com>
2024-06-02 15:17:59 +0900

    API 데이터 클라이언트 사이드에 상태로 반영

--- src/screens/CardList.tsx
+++ src/screens/CardList.tsx
@@ -4,7 +4,7 @@ import Typography from '../components/Typography';
 export default function CardList() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Typography>CardList.tsx</Typography>
+        <Typography>맞춤 정책은 곧 오픈 예정입니다.</Typography>
     </View>
   );
 }
